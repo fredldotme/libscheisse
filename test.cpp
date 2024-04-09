bfd670ade74998e6f5d33fbb6b09598c9064adc5
Alfred E. Neumayer <dev.beidl@gmail.com>
2024-04-09 11:08:07 +0200

    test: Extend testable input string

--- test.cpp
+++ test.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
     time_t current_time;
     srand((unsigned int)time(&current_time));
 
-    const std::string test = "Weil der Dreck nicht geht ist die Scheiße am Dampfen. "
+    const std::string test = "Weil des Thorstens Dreck nicht geht ist die Scheiße am Dampfen. "
                              "Das ist aber i.d.r egal wenn der Hopfen fehlt.";
 
     std::cout << "Eingabe:" << std::endl;
